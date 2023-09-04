f91ee6d071f45cb5f024c5bb10c4a11fc48df2a4
kirandeshmukh6421 <kirandeshmukh2001@gmail.com>
2023-09-04 09:58:21 +0530

    Optimise feature space generation

--- create_feature_space.py
+++ create_feature_space.py
@@ -1,22 +1,28 @@
 import os
 import pandas as pd
 
-csv_directory = 'microtrips/n_k_sunday'
+parent_directory = 'microtrips' 
 
 results = []
 
-for filename in os.listdir(csv_directory):
-    if filename.endswith(".csv"):
-        file_path = os.path.join(csv_directory, filename)
-        df = pd.read_csv(file_path)
+for root, _, files in os.walk(parent_directory):
+    for filename in files:
+        if filename.endswith(".csv"):
+            file_path = os.path.join(root, filename)
+            df = pd.read_csv(file_path)
 
-        avg_speed = df['Speed (km/h)'].mean()
-        total_time = df.shape[0]
-        idle_time = df[df['Speed (km/h)'] == 0].shape[0]
-        idle_time_percentage = (idle_time / total_time) * 100
+            avg_speed = df['Speed (km/h)'].mean()
+            avg_acc = df['Longitudinal acceleration (g)'].mean()
+            total_time = df.shape[0]
+            idle_time = df[df['Speed (km/h)'] == 0].shape[0]
+            idle_time_percentage = (idle_time / total_time) * 100
 
-        results.append({'microtrip': filename, 'avg speed': avg_speed, 'idle time %': idle_time_percentage})
+            results.append({'microtrip': filename, 'avg speed': avg_speed, 'idle time %': idle_time_percentage, 'avg long. acceleration': avg_acc})
 
 results_df = pd.DataFrame(results)
-# results_df.to_csv('results.csv', index=False)
-results_df.to_csv('results.csv', index=False, mode='a', header=False)
+output_csv_path = 'feature_space.csv'
+
+if not os.path.exists(output_csv_path):
+    results_df.to_csv(output_csv_path, index=False)
+else:
+    results_df.to_csv(output_csv_path, index=False, mode='a', header=False)
