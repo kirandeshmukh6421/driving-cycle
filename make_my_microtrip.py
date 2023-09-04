1a2d874218e82e020c40bdf9c552a56fb2b1b4d3
kirandeshmukh6421 <kirandeshmukh2001@gmail.com>
2023-09-04 11:39:10 +0530

    3D clustering of microtrips

--- make_my_microtrip.py
+++ make_my_microtrip.py
@@ -1,42 +1,36 @@
+import os
 import pandas as pd
 import random
 
-df = pd.read_csv('vbox-data/n-k right.csv')
-for i, row in df.iterrows():
-    if df.loc[i, 'Speed (km/h)'] < 1:
-        df.loc[i, 'Speed (km/h)'] = 0
+parent_folder = 'vbox-data'
 
-df2 = df[['Speed (km/h)', 'Elapsed time (s)', 'Longitudinal acceleration (g)']]
+csv_files = [f for f in os.listdir(parent_folder) if f.endswith('.csv')]
 
 min_rows_per_microtrip = 50
 max_rows_per_microtrip = 200
 
-microtrips = []
+for csv_file in csv_files:
+    df = pd.read_csv(os.path.join(parent_folder, csv_file))
 
-while len(df2) > max_rows_per_microtrip:
-    num_rows = random.randint(min_rows_per_microtrip, max_rows_per_microtrip)
-    microtrip = df2.iloc[:num_rows]
-    microtrips.append(microtrip)
-    df2 = df2.iloc[num_rows:]
+    df['Speed (km/h)'] = df['Speed (km/h)'].apply(lambda x: 0 if x < 1 else x)
 
-if not df2.empty:
-    microtrips.append(df2)
+    df2 = df[['Speed (km/h)', 'Elapsed time (s)', 'Longitudinal acceleration (g)']]
 
-for i, microtrip in enumerate(microtrips):
-    file_name = f'microtrips/n_k_right/microtrip_{i + 1}.csv'
-    microtrip.to_csv(file_name, index=False)
+    microtrips = []
 
-# import pandas as pd
+    while len(df2) > max_rows_per_microtrip:
+        num_rows = random.randint(min_rows_per_microtrip, max_rows_per_microtrip)
+        microtrip = df2.iloc[:num_rows]
+        microtrips.append(microtrip)
+        df2 = df2.iloc[num_rows:]
 
-# df = pd.read_csv('n-k sunday.csv')
-# df2 = df[['Speed (km/h)', 'Elapsed time (s)', 'Longitudinal acceleration (g)']]
-# rows = 200
+    if not df2.empty:
+        microtrips.append(df2)
 
-# total_trips = len(df) // rows + 1  
+    output_folder = csv_file.replace('-', '_').replace(' ', '_').replace('.csv', '')
+    output_folder_path = os.path.join('microtrips', output_folder)
+    os.makedirs(output_folder_path, exist_ok=True)
 
-# for i in range(total_trips):
-#     start_index = i * rows
-#     end_index = start_index + rows
-#     micro = df.iloc[start_index:end_index]
-#     file_name = f'microtrips/n_k_sunday/microtrip_{i + 1}.csv'
-#     micro.to_csv(file_name, index=False)
+    for i, microtrip in enumerate(microtrips):
+        file_name = os.path.join(output_folder_path, f'microtrip_{i + 1}.csv')
+        microtrip.to_csv(file_name, index=False)
